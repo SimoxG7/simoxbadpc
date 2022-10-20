8228b1050ab5150016be6ba932412d652a273cd5
SimoxG7 <simone.gioe@studenti.unimi.it>
2022-10-20 20:23:40 +0200

    GREEN: on test multCheck + big refactoring

--- Ingegneria SW/lab03/lab-03-2022-personal/src/main/java/it/unimi/di/sweng/lab03/ForthInterpreter.java
+++ Ingegneria SW/lab03/lab-03-2022-personal/src/main/java/it/unimi/di/sweng/lab03/ForthInterpreter.java
@@ -50,54 +50,6 @@ public class ForthInterpreter implements Interpreter {
         }
     }
 
-    private boolean underflow() {
-        return stack.size() <= 1;
-    }
-
-    private void binaryOp(String op) {
-        switch (op) {
-            case "+":
-                stack.set(index-2, stack.get(index-2) + stack.get(index-1));
-                index--;
-                stack.remove(index);
-                break;
-            case "-":
-                stack.set(index-2, stack.get(index-2) - stack.get(index-1));
-                index--;
-                stack.remove(index);
-                break;
-            case "*":
-                stack.set(index-2, stack.get(index-2) * stack.get(index-1));
-                index--;
-                stack.remove(index);
-                break;
-            case "/":
-                stack.set(index-2, stack.get(index-2) / stack.get(index-1));
-                index--;
-                stack.remove(index);
-                break;
-            case "swap":
-                int temp = stack.get(index-2);
-                stack.set(index-2, stack.get(index-1));
-                stack.set(index-1, temp);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid operand.");
-    }
-
-    private void unaryOp(String op) {
-        if (op.equals("dup")) {
-            stack.set(index++, stack.get(index-1));
-        } else {
-            throw new IllegalArgumentException("Invalid operand");
-        }
-    }
-
-    private void reset() {
-        stack = new ArrayList<>();
-        index = 0;
-    }
-
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -108,6 +60,55 @@ public class ForthInterpreter implements Interpreter {
         return s.toString();
     }
 
+    private void unaryOp(String op) {
+        if (op.equals("dup")) {
+            stack.set(index++, stack.get(index - 1));
+        } else {
+            throw new IllegalArgumentException("Invalid operand");
+        }
+    }
 
+
+    private void reset() {
+        stack = new ArrayList<>();
+        index = 0;
+    }
+
+
+    private boolean underflow() {
+        return stack.size() <= 1;
+    }
+
+    private void binaryOp(String op) {
+        switch (op) {
+            case "+":
+                stack.set(index - 2, stack.get(index - 2) + stack.get(index - 1));
+                index--;
+                stack.remove(index);
+                break;
+            case "-":
+                stack.set(index - 2, stack.get(index - 2) - stack.get(index - 1));
+                index--;
+                stack.remove(index);
+                break;
+            case "*":
+                stack.set(index - 2, stack.get(index - 2) * stack.get(index - 1));
+                index--;
+                stack.remove(index);
+                break;
+            case "/":
+                stack.set(index - 2, stack.get(index - 2) / stack.get(index - 1));
+                index--;
+                stack.remove(index);
+                break;
+            case "swap":
+                int temp = stack.get(index - 2);
+                stack.set(index - 2, stack.get(index - 1));
+                stack.set(index - 1, temp);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operand.");
+        }
+    }
 }
 
