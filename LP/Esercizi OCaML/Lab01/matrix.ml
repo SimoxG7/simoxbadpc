76257a351c55f221583c0243299c86d5c15baa55
SimoxG7 <simone.gioe@studenti.unimi.it>
2022-10-23 19:37:06 +0200

    Added matrix.ml "Heavily inspired" from Sam's solution

--- LP/Esercizi OCaML/Lab01/matrix.ml
+++ LP/Esercizi OCaML/Lab01/matrix.ml
@@ -1,16 +1,31 @@
-type matrix = {l:int; h:int; mutable values: int array array};;
+module Matrix = struct
+type matrix = int list list
 
-let zeroes l h = {l = l; h = h; values = Array.make_matrix l h 0};;
+let zeroes h l = List.init l (fun _ -> List.init h (fun _ -> 0));;
 
-let identity l = {
-  l = l;
-  h = l;
-  values = [|[||]|]
-};;
+let identity h = List.init h (fun x -> List.init h (fun y -> if (x == y) then 1 else 0));;
 
-let a = zeroes 2 2;;
+let init s = List.init s (fun x -> List.init s (fun y -> x*s + y));; 
 
+let rec transpose = function
+  | [] -> []
+  | []::t -> transpose t
+  | (h_n::t_n)::t -> (h_n::List.map List.hd t)::(transpose (t_n :: (List.map List.tl t)))
 
+let list_product l1 l2 =
+  let rec list_product l1 l2 output = match l1, l2 with
+    | h1::t1, h2::t2 -> (list_product t1 t2 (output + h1*h2))
+    | _,_ -> output
+  in list_product l1 l2 0
+;;
+
+let matrix_product m1 m2 = 
+  let prod rowsm1 rowsm2 =
+    let columns = transpose rowsm2 
+  in List.map (fun row -> List.map (list_product row) columns) rowsm1
+in prod m1 m2;;
+
+end
 
 (* type matrix = {l:int ; h:int; mutable values:(int array array)};;
 
