14ce3163bea67ea0a7b1cc4c7552c9a3fdcd42cb
SimoxG7 <simone.gioe@studenti.unimi.it>
2022-11-08 19:20:33 +0100

    added file back

---
+++ SEP/L10/Exercise5.1.c
@@ -0,0 +1,31 @@
+//primo -> stampa x = 10
+
+int x = 0;
+int function() {
+	x = x +13;
+}
+
+void main() {
+	x = function(x);
+	x = 10;
+	printf("Value of x = %d\n", x);
+}
+
+
+//secondo -> stampa x = 13
+int x = 0;
+int function() {
+	int *ret;
+	x = x +13;
+	ret = (int *)&ret + 8; //con questa ret punta alla locazione di stack contenente il return address
+	(*ret) = (*ret) + xx;
+}
+
+void main() {
+	x = function(x);
+	x = 10;
+	printf("Value of x = %d\n", x);
+}
+
+
+
