2acea3d7cca3410bd6ffcb48c20a62841fa1fd9e
SimoxG7 <simone.gioe@studenti.unimi.it>
2022-11-12 13:06:19 +0100

    changed Streams.ml, but it still doesn't work properly cause cazzola did an error

--- LP/OCaML/Lab01/MatrixFuntore/Stream.ml
+++ LP/OCaML/Lab01/MatrixFuntore/Stream.ml
@@ -10,6 +10,12 @@ let tl = function
   | Cons(_, tl) -> tl()
 ;;
 
+let rec take n = function 
+  | Nil -> []
+  | _ when n = 0 -> []
+  | Cons (x, tl) -> x::(take (n-1) (tl()))
+;;
+
 (* generatore di numeri pari *)
 let evens n m = 
   let rec generator i = 
@@ -17,6 +23,9 @@ let evens n m =
     else Nil 
   in Cons(n, fun() -> generator(n+2))
 ;;
+
+take 10 (evens 16 300);;
+take 10 (evens 17 100);;
 
 
 
