65293a91e5111d43ed819d25477acdca73d8777f
SimoxG7 <simone.gioe@studenti.unimi.it>
2022-12-05 12:13:29 +0100

    added some notes of sep L18 and part of leetcodes

---
+++ Extra/leetcode/12. Integer to Roman/Solution.java
@@ -0,0 +1,19 @@
+// import java.util.ArrayList;
+
+// class Solution {
+//   public String intToRoman(int num) {
+    
+//     StringBuilder sb = new StringBuilder();
+
+//     String numString = String.valueOf(num);
+//     int len = numString.length();
+//     ArrayList<Integer> lst = new ArrayList<>();
+
+//     for (int i = 0; i < len; i++) {
+//       lst.add(Integer.parseInt(String.valueOf(numString.charAt(i))));
+//     }
+
+//     System.out.println();
+//     return sb.toString();
+//   }
+// }
