aafb274f926ab4db41f69d6a42c2293445e5b83c
SimoxG7 <simone.gioe@studenti.unimi.it>
2022-12-05 12:56:32 +0100

    added leetcode sol

--- Extra/leetcode/12. Integer to Roman/solution.c
+++ Extra/leetcode/12. Integer to Roman/solution.c
@@ -1,3 +1,19 @@
-char * intToRoman(int num){
+char * intToRoman(int num)
+{
+    char symbols[4][10][5] = {
+                {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+                {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+                {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+                {"", "M", "MM", "MMM"}};
+  
+    char *res = calloc(sizeof(char), 16);
 
+    for (int i = 0; num > 0; num = num / 10, i++) {
+        int d = num % 10;
+        char *tmp = strdup(res);
+        sprintf(res, "%s%s", symbols[i][d], tmp);
+        free(tmp);
+    }
+
+    return res;
 }
