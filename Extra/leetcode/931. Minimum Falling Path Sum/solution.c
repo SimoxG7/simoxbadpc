62d73e7af87f95e754a3a9ba0052894d61cad92d
Simox <simone.gioe@studenti.unimi.it>
2022-12-13 13:22:26 +0100

    finished rdc lab12 notes and pt

--- Extra/leetcode/931. Minimum Falling Path Sum/solution.c
+++ Extra/leetcode/931. Minimum Falling Path Sum/solution.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 //https://leetcode.com/problems/minimum-falling-path-sum/
 int minFallingPathSum(int** matrix, int matrixSize, int* matrixColSize){
+  int max = INT_MIN; 
 
+  for (int i = 0; i < matrixSize; i++) {
+    //per ogni elemento della prima riga
+    int curr = INT_MIN;
+    int prev = i;
+    for (int j = 0; j < matrixSize; j++) {
+      //guardo riga per riga
+      
+      if (j > 0) {
+        if (matrix[i+1][j-1]) return 0;
+      }
+    }
+  }
 
 
 }
