3bd14be05de161e89884fb32fe89e27e7d36b904
Simox <simone.gioe@studenti.unimi.it>
2022-12-15 12:24:57 +0100

    tried a new idea in the leetcode

--- Extra/leetcode/1143. Longest Common Subsequence/Solution.java
+++ Extra/leetcode/1143. Longest Common Subsequence/Solution.java
@@ -3,32 +3,46 @@ class Solution {
     int sub = 0;
     StringBuilder sb = new StringBuilder();
 
-    if (text1.length() > text2.length()) {
+    if (text1.length() < text2.length()) {
       String temp = text1;
       text1 = text2;
       text2 = temp;
     }
 
-    for (int i = 0, j = i; i < text1.length(); i++) {
-      while (j < text2.length() && text1.charAt(i) != text2.charAt(j)) {
-        j++;
-      }
+    int j = 0;
+    int len = text1.length();
 
-      if (j >= text2.length()) break;
-      sb.append(text2.charAt(j));
-      sub++;
-      j++;
-
+    for (int i = 0; i < text2.length(); i++) {
+      if (text1.substring(j, len).contains(String.valueOf(text2.charAt(i)))) {
+        sb.append(text2.charAt(i));
+        j += text1.substring(j, len).indexOf(String.valueOf(text2.charAt(i))) +1;
+        sub++;
+      } 
     }
-
     System.out.println(sb.toString());
 
-    if (sub == 0) {
-      for (int i = 0; i < text2.length(); i++) {
-        if (text1.contains(String.valueOf(text2.charAt(i)))) return 1;
-      }
-    }
-    
     return sub;
   }
 }
+
+/*
+ * for (int i = 0, j = i; i < text1.length(); i++) {
+ * while (j < text2.length() && text1.charAt(i) != text2.charAt(j)) {
+ * j++;
+ * }
+ * 
+ * if (j >= text2.length()) break;
+ * sb.append(text2.charAt(j));
+ * sub++;
+ * j++;
+ * 
+ * }
+ * 
+ * System.out.println(sb.toString());
+ * 
+ * if (sub == 0) {
+ * for (int i = 0; i < text2.length(); i++) {
+ * if (text1.contains(String.valueOf(text2.charAt(i)))) return 1;
+ * }
+ * }
+ */
