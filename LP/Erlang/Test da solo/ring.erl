4cfa29bcbdecf606520c35e596b24ae1400143c6
Simox <simone.gioe@studenti.unimi.it>
2022-12-18 00:42:46 +0100

    fuck erlang pt2

--- LP/Erlang/Test da solo/ring.erl
+++ LP/Erlang/Test da solo/ring.erl
@@ -2,52 +2,182 @@
 -compile(export_all).
 % -export([]).
 
+
 start(N) -> 
-  register(server, spawn(?MODULE, create_ring, [N])).
+  register(rring_fst, spawn(?MODULE, create, [N, self()])), %registro il primo nodo creato, che creerà gli altri
+  receive 
+    ready -> ok
+    after 5000 -> exit(timeout)
+  end,
+  %wait for command 
+  register(server, spawn(?MODULE, commandloop, []))
+.
 
-create_ring(N) ->
-  register(first_ring, generate(N, self())),
-  first_loop(first_ring).
+commandloop() -> 
+  io:format("Server: ~p~n", [whereis(server)]),
+  receive 
+    {send, Msg} -> 
+      rring_fst ! {Msg},
+      commandloop();
+    stop -> 
+      rring_fst ! stop ,
+      exit(normal)
+  end. 
 
-
-generate(0, Starter) -> 
+create(1, Starter) -> 
   Starter ! ready,
-  loop(Starter);
-generate(N, Starter) -> 
-  Next = spawn(?MODULE, generate, [N-1, Starter]),
+  loop_last(rring_fst);
+create(N, Starter) -> 
+  Next = spawn(?MODULE, create, [N-1, Starter]),
   loop(Next).
 
-
-first_loop(Next) -> 
-  receive 
-    ready -> ok,
-      first_loop(Next);
-    {Msg} -> 
-      io:format("Sending msg to next ~p from ~p~n", [Next, self()]),
-      Next ! Msg,
-      first_loop(Next);
-    stop ->
-      Next ! stop, 
-      exit(normal);
-    Other -> 
-      io:format("Received other: ~p [first_loop]~n", [Other]),
-      first_loop(Next)
-  end.
 
 loop(Next) -> 
   receive 
     {Msg} -> 
-      io:format("Sending msg to next ~p from ~p~n", [Next, self()]),
-      Next ! Msg,
+      io:format("Process ~p received message ~p~n", [self(), Msg]),
+      Next ! {Msg},
       loop(Next);
-    stop -> 
+    stop ->
       Next ! stop,
       exit(normal);
-    Other -> 
-      io:format("Received other: ~p [loop]~n", [Other]),
-      loop(Next)
+    Other -> io:format("*** received other stuff: ~p ***~n", [Other])
+  end.
+
+loop_last(Next) -> 
+  receive 
+    {Msg} -> 
+      io:format("Process ~p received message ~p~n", [self(), Msg]),
+      %Next ! {Msg},
+      io:format("Ring completed~n"),
+      loop_last(Next);
+    stop -> 
+      exit(normal),
+      unregister(rring_fst);
+    Other -> io:format("*** received other stuff: ~p ***~n", [Other])
   end.
 
 test(Msg) -> 
-  whereis(first_ring) ! {Msg}.
+  server ! {send, Msg}.
+
+stop() -> 
+  unregister(server),
+  unregister(rring_fst),
+  server ! stop.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% start(N) -> 
+%   io:format("Start: ~p~n", [self()]),
+%   register(first, spawn(?MODULE, create, [N])).
+
+% create(0) -> 
+%   io:format("Ring completed~n"),
+%   spawn(?MODULE, loop_last, []);
+% create(N) -> 
+%   io:format("Ring process: ~p~n", [self()]),
+%   Next = spawn(?MODULE, create, [N-1]),
+%   spawn(?MODULE, loop, [Next]).
+
+% loop(Next) -> 
+%   receive 
+%     {Msg} -> 
+%       io:format("Process ~p received message <~p>~n", [self(), Msg]),
+%       Next ! Msg,
+%       loop(Next);
+%     Other -> 
+%       io:format("Process ~p received message <~p>~n", [self(), Other]),
+%       loop(Next)
+%   end. 
+
+% loop_last() ->
+%   receive 
+%     {Msg} -> 
+%       io:format("Process ~p received message <~p>~n", [self(), Msg]),
+%       loop_last();
+%     Other -> 
+%       io:format("Process ~p received message <~p>~n", [self(), Other]),
+%       loop_last()
+%   end. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% start(N) -> 
+%   register(server, spawn(?MODULE, create_ring, [N])).
+
+% create_ring(N) ->
+%   register(first_ring, spawn(?MODULE, generate, [N, self()])),
+%   first_loop(first_ring).
+
+
+% generate(0, Starter) -> 
+%   Starter ! ready,
+%   loop(Starter);
+% generate(N, Starter) ->
+%   io:format("Spawned process ~p~n", [self()]), 
+%   Next = spawn(?MODULE, generate, [N-1, Starter]),
+%   loop(Next).
+
+
+% first_loop(Next) -> 
+%   receive 
+%     ready -> ok,
+%       first_loop(Next);
+%     {Msg} -> 
+%       io:format("Sending msg to next ~p from ~p [first_loop]  ~n", [Next, self()]),
+%       Next ! Msg,
+%       first_loop(Next);
+%     stop ->
+%       Next ! stop, 
+%       exit(normal);
+%     Other -> 
+%       io:format("Received other: ~p [first_loop]~n", [Other]),
+%       first_loop(Next)
+%   end.
+
+% loop(Next) -> 
+%   receive 
+%     {Msg} -> 
+%       io:format("Sending msg to next ~p from ~p~n", [Next, self()]),
+%       Next ! Msg,
+%       loop(Next);
+%     stop -> 
+%       Next ! stop,
+%       exit(normal);
+%     Other -> 
+%       io:format("Received other: ~p [loop ~p]~n", [Other, self()]),
+%       loop(Next)
+%   end.
+
+% test(Msg) -> 
+%   server ! {Msg}.
 
--- LP/Erlang/lab1/Soluzioni del prof/ring/rring_no_io.erl
+++ LP/Erlang/lab1/Soluzioni del prof/ring/rring_no_io.erl
@@ -2,27 +2,20 @@
 -module(rring_no_io).
 -export([start/3, create/3]).
 
-%numero di messaggi, numero di nodi, messaggio
 start(M, N, Msg) -> 
-  %diamo un ruolo speciale al primo creato e all'ultimo creato, poichè si devono connettere
   register(rring_fst, spawn(?MODULE, create, [N, 1, self()])), %registro il primo nodo creato, che creerà gli altri
-  %andiamo in attesa
   receive 
     ready -> ok
     after 5000 -> exit(timeout)
   end,
-  %una volta che il ring è pronto, iniziamo a far mandare i messaggi, dopodichè fermiamo.
   msg_dispatcher(M, 1, Msg), 
   rring_fst ! stop, 
   ok
 .
 
-%caso in cui ho creato tutti i processi
 create(1, Who, Starter) -> 
   Starter ! ready,
-  %looplast prende identificativo del next e identificato del prossimo processo
   loop_last(rring_fst, Who);
-%Who è il progressivo che gli abbiamo dato.
 create(N, Who, Starter) -> 
   Next = spawn(?MODULE, create, [N-1, Who+1, Starter]),
   loop(Next, Who).
@@ -30,7 +23,6 @@ create(N, Who, Starter) ->
 
 loop(Next, Who) -> 
   receive 
-    %N è numero del messaggio, Pass è quante volte è già passato
     {Msg, N, Pass} -> 
       Next ! {Msg, N, Pass},
       loop(Next, Who);
@@ -46,13 +38,10 @@ loop_last(Next, Who) ->
       loop_last(Next, Who);
     stop -> 
       exit(normal),
-      %tolgo dai registrati il ring, non devo neanche dare lo stop perchè il primo nodo 
-      %teoricamente è già uscito
       unregister(rring_fst);
     Other -> io:format("*** received other stuff: ~p ***~n", [Other])
   end.
 
-% ho mandato M messaggi, dunque mando l'ultimo
 msg_dispatcher(M, M, Msg) -> rring_fst ! {Msg, M, 1};
 msg_dispatcher(M, N, Msg) -> rring_fst ! {Msg, N, 1}, msg_dispatcher(M, N+1, Msg).
 
