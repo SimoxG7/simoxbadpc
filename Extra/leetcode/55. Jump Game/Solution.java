1abb98c3077e3d2325b161eb28d87456fa402a18
Simox <simone.gioe@studenti.unimi.it>
2022-12-26 10:45:42 +0100

    added leetcode

---
+++ Extra/leetcode/55. Jump Game/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  public boolean canJump(int[] nums) {
+    if (nums.length == 1) return true;
+
+    int target = nums.length-1;
+    for (int i = nums.length-2; i >= 0; i--) {
+      if (i + nums[i] >= target) {
+        target = i;
+      }
+      if (target == 0) return true;
+    }
+    return false;
+  }
+}
