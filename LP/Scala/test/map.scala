dedbf57520be6db295ffb134b963bd75961c72e6
Simox <simone.gioe@studenti.unimi.it>
2022-12-29 18:10:59 +0100

    created my list.map in scala

--- LP/Scala/test/map.scala
+++ LP/Scala/test/map.scala
@@ -3,10 +3,9 @@ object Map {
 
   def addOne(n:Int):Int = n+1
 
-  var list = new ListBuffer[Int]
-  list += 1
-  list += 2 
-  list += 3
-  var newlist = list.foreach(num => addOne(num))
+  def map[A, B](f:A => B, start:List[A]):List[B] = {
+    for (elem <- start) yield f(elem)
+  } 
 
+  var newlist:List[Int] = map(((num:Int) => num * 2), List(1,2,3)) 
 }
