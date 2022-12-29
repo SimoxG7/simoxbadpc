eacf7cb5d6f9e62ec5dda62b1db7cdfd739f4b15
Simox <simone.gioe@studenti.unimi.it>
2022-12-29 12:07:21 +0100

    added scala leetcode

--- Extra/leetcode/9. Palindrome Number/Solution.scala
+++ Extra/leetcode/9. Palindrome Number/Solution.scala
@@ -1,9 +1,5 @@
 object Solution {
   def isPalindrome(x: Int): Boolean = {
-    isPal(x.toString())  
-  }
-
-  def isPal(s:String):Boolean = {
-    
+    x.toString().equals(x.toString().reverse)  
   }
 }
