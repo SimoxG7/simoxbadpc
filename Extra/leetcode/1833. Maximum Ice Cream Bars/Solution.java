69dfa0e05d65086b002cbbefe4639691472c2696
Simox <simone.gioe@studenti.unimi.it>
2023-01-06 10:28:47 +0100

    added leetcode

---
+++ Extra/leetcode/1833. Maximum Ice Cream Bars/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+  public int maxIceCream(int[] costs, int coins) {
+    Arrays.sort(costs);
+    int count = 0;
+    for (int i = 0; i < costs.length; i++) {
+      if (costs[i] <= coins) {
+        coins -= costs[i];
+        count++;
+      } else break;
+    } 
+    return count;
+  }
+}
