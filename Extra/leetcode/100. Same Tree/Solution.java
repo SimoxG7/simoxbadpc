07d6cc5a1e15323363eed50e2e9aca21d52a090c
Simox <simone.gioe@studenti.unimi.it>
2023-01-10 10:55:35 +0100

    added leetcode

---
+++ Extra/leetcode/100. Same Tree/Solution.java
@@ -0,0 +1,32 @@
+
+class Solution {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null)
+      return true;
+    if (q == null || p == null)
+      return false;
+    if (p.val != q.val)
+      return false;
+    return (isSameTree(p.right, q.right) && isSameTree(p.left, q.left));
+  }
+}
