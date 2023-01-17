b076c841d52cc6fc571b54137d34e7882f8a7da6
Simox <simone.gioe@studenti.unimi.it>
2023-01-17 21:07:38 +0100

    added a new file

--- LP/Scala/parsers/JSON.scala
+++ LP/Scala/parsers/JSON.scala
@@ -1,8 +1,16 @@
 import scala.util.parsing.combinator._
+import java.io.FileReader
 
 class JSON extends JavaTokenParsers {
   def value : Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
   def obj : Parser[Any] = "{"~repsep(member, ",")~"}"
   def arr : Parser[Any] = "["~repsep(value, ",")~"]"
   def member: Parser[Any] = stringLiteral~":"~value
+}
+
+object ParseJSON extends JSON {
+  def main(args: Array[String]) {
+    val reader = new FileReader(args(0))
+    println(parseAll(value, reader))
+  }
 }
