07e8792e5b064a797b8ebf0e81132d8b0e77dd4b
simoxg7 <simone.gioe@studenti.unimi.it>
2023-01-28 13:58:43 +0100

    added leetcode

---
+++ Extra/leetcode/352. Data Stream as Disjoint Intervals/SummaryRanges.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+class SummaryRanges {
+  private Set<Integer> values;
+
+  public SummaryRanges() {
+    values = new TreeSet<>();
+  }
+
+  public void addNum(int value) {
+    values.add(value);
+  }
+
+  public int[][] getIntervals() {
+    if (values.isEmpty()) {
+      return new int[0][2];
+    }
+    List<int[]> intervals = new ArrayList<>();
+    int left = -1, right = -1;
+    for (Integer value : values) {
+      if (left < 0) {
+        left = right = value;
+      } else if (value == right + 1) {
+        right = value;
+      } else {
+        intervals.add(new int[] { left, right });
+        left = right = value;
+      }
+    }
+    intervals.add(new int[] { left, right });
+    return intervals.toArray(new int[0][]);
+  }
+}
